0b03de10ed9dd607d8521a70bb4433781ed1b5f3
Johan Blomgren <johan.blomgren@leogears.com>
2015-11-17 09:27:06 +0100

    UPDATES module name and native signature

--- www/IndexAppContent.js
+++ www/IndexAppContent.js
@@ -1,16 +1,16 @@
 var exec = require("cordova/exec");
 
-var InAppContent = function () {
+var IndexAppContent = function () {
 };
 
-InAppContent.prototype.setItems = function (items, onSuccess, onError) {
-  exec(onSuccess, onError, "InAppContent", "setItems", [items]);
+IndexAppContent.prototype.setItems = function (items, onSuccess, onError) {
+  exec(onSuccess, onError, "IndexAppContent", "setItems", [items]);
 };
 
-module.exports = new InAppContent();
+module.exports = new IndexAppContent();
 
 // call the plugin as soon as deviceready fires, this makes sure the webview is loaded,
 // way more solid than relying on native's pluginInitialize.
 document.addEventListener('deviceready', function() {
-  exec(null, null, "InAppContent", "deviceIsReady", []);
+  exec(null, null, "IndexAppContent", "deviceIsReady", []);
 }, false);

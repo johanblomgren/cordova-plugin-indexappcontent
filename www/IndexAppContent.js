6d51d9abeb0f91079a2a339b977d41c542bb1438
Johan Blomgren <johan.blomgren@leogears.com>
2016-02-24 16:37:42 +0100

    UPDATES js plugin install

--- www/IndexAppContent.js
+++ www/IndexAppContent.js
@@ -1,24 +1,29 @@
 cordova.define("cordova-plugin-indexappcontent", function(require, exports, module) {
-  var exec = require("cordova/exec");
+	var exec = require("cordova/exec");
 
-  var IndexAppContent = function () {
-  };
+	var IndexAppContent = function () {};
 
-  IndexAppContent.prototype.init = function (onSuccess, onError) {
-    exec(null, null, "IndexAppContent", "deviceIsReady", []);
-  };
+	IndexAppContent.prototype.init = function (onSuccess, onError) {
+		exec(null, null, "IndexAppContent", "deviceIsReady", []);
+	};
 
-  IndexAppContent.prototype.setItems = function (items, onSuccess, onError) {
-    exec(onSuccess, onError, "IndexAppContent", "setItems", [items]);
-  };
-  
-  IndexAppContent.prototype.clearItemsForDomains = function (domains, onSuccess, onError) {
-    exec(onSuccess, onError, "IndexAppContent", "clearItemsForDomains", [domains]);
-  };
-  
-  IndexAppContent.prototype.setIndexingInterval = function (interval, onSuccess, onError) {
-    exec(onSuccess, onError, "IndexAppContent", "setIndexingInterval", [interval]);
-  };
+	IndexAppContent.prototype.setItems = function (items, onSuccess, onError) {
+		exec(onSuccess, onError, "IndexAppContent", "setItems", [items]);
+	};
 
-  module.exports = new IndexAppContent();
+	IndexAppContent.prototype.clearItemsForDomains = function (domains, onSuccess, onError) {
+		exec(onSuccess, onError, "IndexAppContent", "clearItemsForDomains", [domains]);
+	};
+
+	IndexAppContent.prototype.setIndexingInterval = function (interval, onSuccess, onError) {
+		exec(onSuccess, onError, "IndexAppContent", "setIndexingInterval", [interval]);
+	};
+
+	if (!window.plugins) {
+		window.plugins = {};
+	}
+
+	window.plugins.indexAppContent = new IndexAppContent();
+
+	module.exports = window.plugins.indexAppContent;
 });

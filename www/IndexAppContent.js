097bf9acba98325ee108bb99520848593de1a4f5
Johan Blomgren <johan.blomgren@leogears.com>
2016-02-24 16:31:43 +0100

    UPDATES plugin id

--- www/IndexAppContent.js
+++ www/IndexAppContent.js
@@ -1,4 +1,4 @@
-cordova.define("cordova-plugin-indexappcontent.IndexAppContent", function(require, exports, module) {
+cordova.define("cordova-plugin-indexappcontent", function(require, exports, module) {
   var exec = require("cordova/exec");
 
   var IndexAppContent = function () {
